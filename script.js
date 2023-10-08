7df489e8977bac421e8a20855c50f0aa896bc1f2
sieun <charm516@naver.com>
2023-10-09 01:11:13 +0900

    주석 추가

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 const imageInput1 = document.getElementById("imageInput1");
 const imageInput2 = document.getElementById("imageInput2");
 const imageCanvas = document.getElementById("imageCanvas");
@@ -58,20 +57,22 @@ function loadImage(file, imageNumber) {
 }
 
 function applyXOREffect() {
+  // 이미지 데이터 2개가 모두 로드되었으면 if문 실행
   if (image1Data && image2Data) {
+    // 결과 이미지 데이터 생성, 결과 이미지 크기 == 캔버스 크기
     const resultData = ctx.createImageData(
       imageCanvas.width,
       imageCanvas.height
     );
-
+    // 이미지 픽셀 순화하면서 XOR 연산 수행
     for (let i = 0; i < image1Data.data.length; i += 4) {
-      // XOR
+      // image1과 image2의 각 픽셀의 R,G,B에 대해 XOR 연산 수행
       resultData.data[i] = image1Data.data[i] ^ image2Data.data[i]; // R
       resultData.data[i + 1] = image1Data.data[i + 1] ^ image2Data.data[i + 1]; // G
       resultData.data[i + 2] = image1Data.data[i + 2] ^ image2Data.data[i + 2]; // B
       resultData.data[i + 3] = 255;
     }
-
+    // 연산 결과 캔버스에 표시
     ctx.putImageData(resultData, 0, 0);
   }
 }
